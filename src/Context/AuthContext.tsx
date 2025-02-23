f62f7a272db2d2d2794b7b0bb9f71063022b9a86
furkanmemis <fmemis0160@gmail.com>
2025-02-23 21:10:01 +0300

    Updated profile and authentication

--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
 import { SessionUser } from "../Models/SessionUser";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 
 interface AuthContextType {
@@ -10,6 +9,7 @@ interface AuthContextType {
   name: string;
   surname: string;
   email: string;
+  role: string;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -31,30 +31,30 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [name, setName] = useState<string>('');
   const [surname,setSurname] = useState<string>('');
   const [email, setEmail] = useState<string>('');
+  const [role, setRole] = useState<string>('');
 
   const login = async (userData: SessionUser) => {
     try {
-      const password = userData.password;
-      const email = userData.email;
-
-      const response = await axios.post("http://localhost:4000/auth/login", {
-        email,
-        password,
-      });
+      const { email, password } = userData;
+      const response = await axios.post("http://localhost:4000/auth/login", { email, password });
+  
+      console.log("login response ->", response);
       const user = response.data.user;
-
+  
       if (response.data.result === "Success") {
-        localStorage.setItem("sessionUser", user);
-        console.log("user info")
-        console.log(user.name)
-        console.log(user.surname)
-        console.log(user.email)
-
-
-        window.location.href = "/home";
-        setName(user.name);
-        setEmail(user.email);
-        setSurname(user.surname);
+        localStorage.setItem("sessionUser", JSON.stringify(user));
+  
+        console.log("user info", user.name, user.surname, user.email);
+        setName(user.name)
+        setSurname(user.surname)
+        setEmail(user.email)
+        setRole(user.role);
+  
+        // URL'yi değiştir
+        window.history.pushState({}, "", "/home");
+  
+        // Yeni sayfa içeriğini yüklemek için durumu güncelle (Opsiyonel)
+        window.dispatchEvent(new Event("popstate"));
       }
     } catch (error) {
       console.log("login error: ", error);
@@ -66,7 +66,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{login, logout, name, surname, email }}>
+    <AuthContext.Provider value={{login, logout, name, surname, email, role }}>
       {children}
     </AuthContext.Provider>
   );
--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,20 +1,48 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Grid from "@mui/material/Grid2";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import { useAuth } from "../Context/AuthContext";
 
 const Profile: React.FC = () => {
+  const { email, name, surname, role } = useAuth();
 
-    return (
-        <Grid container  style={{ height: "100vh" }}>
-
-            <Grid size={12} textAlign="center" sx={{marginTop: 3}}>
-
-                <Typography variant="h4" style={{fontFamily: "monospace", color: "darkslateblue"}}>Profile</Typography>
-
-            </Grid>
-
+  return (
+    <Grid container style={{ height: "100vh", flexDirection: "column" }}>
+      <Grid size={12} textAlign="center">
+        <Typography
+          variant="h4"
+          style={{ fontFamily: "monospace", color: "darkslateblue" }}
+        >
+          Profile
+        </Typography>
+      </Grid>
+      <Grid size={12} sx={{ marginLeft: 4 }}>
+        <Grid size={12}>
+          <Typography
+            variant="h5"
+            sx={{ fontFamily: "monospace", color: "darkslateblue" }}
+          >
+            User Information
+          </Typography>
         </Grid>
-    );
+        <Grid size={12}>
+          <Typography variant="h6">Name: {name}</Typography>
+        </Grid>
+        <Grid size={12}>
+          <Typography variant="h6">Surname: {surname}</Typography>
+        </Grid>
+
+        <Grid size={12}>
+          <Typography variant="h6">Email: {email}</Typography>
+        </Grid>
+        {role === "admin" ? (
+          <Grid size={12}>
+            <Typography variant="h6">Role: {role}</Typography>
+          </Grid>
+        ) : null}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Profile;
